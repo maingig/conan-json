ef38d36f71e6d6c23d3a14caec54089f083a1fd4
Fred Malouf <fred.malouf@nio.io>
2020-08-20 09:49:46 -0700

    First commit

---
+++ conanfile.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from conans import ConanFile, CMake, tools
+
+class SigslotConan(ConanFile):
+    name = "json"
+    version = "3.1.9"
+    license = "https://github.com/nlohmann/json/blob/develop/LICENSE.MIT"
+    author = "https://github.com/nlohmann/json/graphs/contributors"
+    url = "https://github.com/nlohmann/json.git"
+    description = "header-only, thread safe implementation of json for C++"
+    no_copy_source = True
+
+    def source(self):
+        self.run("git clone %s %s" % (self.url, self.name))
+        self.run("cd %s && git checkout tags/v%s master" % (self.name), (self.version))
+
+    def package(self):
+        self.copy("*.hpp", src=self.name)
+
+    def package_id(self):
+        self.info.header_only()
+        
+
+
